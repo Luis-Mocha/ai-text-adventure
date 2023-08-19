4fb22a36e98bdc1c716a728239f201e5d135f14f
Luis-Mocha <luismocha97@gmail.com>
2023-08-19 17:50:27 +0200

    file .env

--- src/pages/Playground.vue
+++ src/pages/Playground.vue
@@ -2,18 +2,34 @@
 
 export default {
   name: 'Playground',
-  
+  data() {
+    return {
+
+    }
+  },
+  created() {
+    this.test()
+  },
+  methods: {
+    test() {
+      console.log(`ehi ${import.meta.env.VITE_API_KEY}`);
+    },
+  }
 }
 
 </script>
 
 <template>
 
-  <h1 class="text-center text-success">Playground</h1>
+  <h1 class="text-center">Playground</h1>
 
 </template>
 
 <style lang="scss" scoped>
 @use '../style/main.scss';
+
+h1 {
+  color: green;
+}
 
 </style>
