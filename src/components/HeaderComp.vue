ab51a08eb0ae83db55b2799c2a30aa4dfc99e914
Luis-Mocha <luismocha97@gmail.com>
2023-08-28 17:43:07 +0200

    style title

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -10,7 +10,13 @@
 <template>
 
     <header>
-        <div>Text Adventure</div>
+        
+        <div class="container">
+            <a href="/">
+                <span>Ai.</span>Venture
+            </a>
+            
+        </div>
     </header>
     
 
@@ -20,12 +26,19 @@
 @use '../style/main.scss';
 
 header {
-    // background-color: rgb(25, 64, 30);
     background: linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(18,73,39,1) 100%);
-    // background-color: transparent;
     color: white;
-    font-size: 40px;
 
     height: 60px;
+
+    a {
+        font-size: 50px;
+        letter-spacing: 4px;
+
+        span {
+            font-size: 40px;
+            font-style: italic;
+        }
+    }
 }
 </style>
